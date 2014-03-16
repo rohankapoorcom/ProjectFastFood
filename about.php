c803456428efd2491b6f0aa47b20a0019058a579
Rohan Kapoor <rohan@rohankapoor.com>
2014-03-16 04:29:50 -0500

    Setting up more pages and css overrides

---
+++ about.php
@@ -0,0 +1,11 @@
+<?php
+	require_once './vendor/autoload.php';
+	include_once './includes/db-connect.php';
+
+	$loader = new Twig_Loader_Filesystem('./templates');
+	$twig = new Twig_Environment($loader);
+
+	
+
+	echo $twig->render('about.html');
+?>
