0b8f6d4013501b90c1d9d7cfe1c10da19261572c
Rohan Kapoor <rohan@rohankapoor.com>
2014-03-16 22:33:45 -0500

    Order placement implemented!

---
+++ includes/process_order.php
@@ -0,0 +1,69 @@
+<?php
+include_once './db-connect.php';
+include_once './functions.php';
+
+session_start();
+
+if (isset($_POST['street'], $_POST['zipcode'], $_POST['restaurant'], $_POST['price'],
+	$_POST['time-placed'], $_POST['time-delivered'])) {
+
+	$street = filter_input(INPUT_POST, 'street', FILTER_SANITIZE_STRING);
+	$zipcode = filter_input(INPUT_POST, 'zipcode', FILTER_SANITIZE_STRING);
+	$restaurant = filter_input(INPUT_POST, 'restaurant', FILTER_SANITIZE_STRING);
+	$price = filter_input(INPUT_POST, 'price', FILTER_SANITIZE_NUMBER_FLOAT);
+
+	$time_p = $_POST['time-placed'];
+	$time_p[10] = ' ';
+	$time_p = date_create_from_format('Y-m-d H:i', $time_p);
+	$time_placed = $time_p->format('Y-m-d H:i:s');
+
+	$time_d = $_POST['time-delivered'];
+	$time_d[10] = ' ';
+	$time_d = date_create_from_format('Y-m-d H:i', $time_d);
+	$time_delivered = $time_d->format('Y-m-d H:i:s');
+
+
+	$prep_stmt = "SELECT id FROM locations WHERE street = ? AND zipcode = ?";
+	$stmt = $mysql_con->prepare($prep_stmt);
+
+	$location_id;
+
+	if ($stmt) {
+		$stmt->bind_param('si', $street, $zipcode);
+		$stmt->execute();
+		$stmt->store_result();
+
+		if ($stmt->num_rows == 1) {
+			// We have the id
+			$stmt->bind_result($location_id);
+			$stmt->fetch();
+		}
+
+		else {
+			// we manufacture a location
+			$prep_stmt = "INSERT INTO locations (address, zip) VALUES(?, ?)";
+			$stmt = $mysql_con->prepare($prep_stmt);
+
+			if ($stmt) {
+			$stmt->bind_param('si', $street, $zipcode);
+			$stmt->execute();
+			$location_id = mysqli_insert_id($mysql_con);
+
+			}
+
+		}
+	}
+
+	$prep_stmt = "INSERT INTO orders (location, restaurant, userID, price, timePlaced, timeArrived)
+		VALUES(?, ?, ?, ?, ?, ?)";
+	$stmt = $mysql_con->prepare($prep_stmt);
+
+	$uid = $_SESSION['user_id'];
+
+	if ($stmt) {
+		$stmt->bind_param('iiidss', $location_id, $restaurant, $uid, $price, $time_placed, $time_delivered);
+		$stmt->execute();
+
+		header('Location: /');
+	}
+}
---
+++ order.php
@@ -0,0 +1,45 @@
+<?php
+	require_once './vendor/autoload.php';
+	include_once './includes/db-connect.php';
+	include_once './includes/functions.php';
+
+	$loader = new Twig_Loader_Filesystem('./templates');
+	$twig = new Twig_Environment($loader);
+
+	session_start();
+
+	if (login_check($mysql_con) == true) {
+		$id = $_SESSION['user_id'];
+
+		$stmt = $mysql_con->prepare("SELECT name FROM users WHERE id = ? LIMIT 1");
+		$stmt->bind_param('i', $id);
+		$stmt->execute();
+		$stmt->store_result();
+
+		if ($stmt->num_rows == 1) {
+			$stmt->bind_result($username);
+			$stmt->fetch();
+		}
+
+	}
+
+	else {
+		header('Location: /login/');
+		exit();
+	}
+
+	$query = "SELECT name, id from restaurants";
+
+	$rests;
+	$i = 0;
+
+	if ($result = $mysql_con->query($query)) {
+		while ($row = $result->fetch_object()) {
+			$rests[$i++] = $row;
+		}
+
+		$result->close();
+	}
+
+	echo $twig->render('order.html', array('username' => $username, "rests" => $rests));
+?>
