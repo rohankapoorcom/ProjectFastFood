ee6202583f8fcf053cc9baa3c9ecd43ff5f48fa3
Rohan Kapoor <rohan@rohankapoor.com>
2014-03-16 19:13:17 -0500

    Header fixes when logged in on the about page

--- about.php
+++ about.php
@@ -1,11 +1,29 @@
 <?php
 	require_once './vendor/autoload.php';
 	include_once './includes/db-connect.php';
+	include_once './includes/functions.php';
 
 	$loader = new Twig_Loader_Filesystem('./templates');
 	$twig = new Twig_Environment($loader);
 
+	session_start();
+
+	if (login_check($mysql_con) == true) {
+		$id = $_SESSION['user_id'];
+
+		$stmt = $mysql_con->prepare("SELECT name FROM users WHERE id = ? LIMIT 1");
+		$stmt->bind_param('i', $id);
+		$stmt->execute();
+		$stmt->store_result();
+
+		if ($stmt->num_rows == 1) {
+			$stmt->bind_result($username);
+			$stmt->fetch();
+		}
+
+	}
+
 	
 
-	echo $twig->render('about.html');
+	echo $twig->render('about.html', array('username' => $username));
 ?>
