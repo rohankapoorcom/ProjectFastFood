914504a24ee242a0b77476b567411d064d7a45ee
Rohan Kapoor <rohan@rohankapoor.com>
2014-03-23 00:03:33 -0400

    Implemented the ability to add restaurants

---
+++ add_restaurant.php
@@ -0,0 +1,47 @@
+<?php
+	require_once './vendor/autoload.php';
+	include_once './includes/db-connect.php';
+	include_once './includes/functions.php';
+
+	$loader = new Twig_Loader_Filesystem('./templates');
+	$twig = new Twig_Environment($loader);
+
+	$username = "";
+
+	session_start();
+
+	if (login_check($mysql_con) == true) {
+		$id = $_SESSION['user_id'];
+
+		$stmt = $mysql_con->prepare("SELECT name FROM users WHERE id = ? LIMIT 1");
+		$stmt->bind_param('i', $id);
+		$stmt->execute();
+		$stmt->store_result();
+
+		if ($stmt->num_rows == 1) {
+			$stmt->bind_result($username);
+			$stmt->fetch();
+		}
+	}
+
+	else {
+		$_SESSION['login_redir'] = substr($_SERVER["REQUEST_URI"], 1);
+		header('Location: /login/');
+		exit();
+	}
+
+	$query = "SELECT name, id FROM categories";
+
+	$categories = "";
+	$i = 0;
+
+	if ($result = $mysql_con->query($query)) {
+		while ($row = $result->fetch_object()) {
+			$categories[$i++] = $row;
+		}
+
+		$result->close();
+	}
+
+	echo $twig->render('add_restaurant.html', array('username' => $username, 'categories' => $categories));
+?>
---
+++ includes/process_add_restaurant.php
@@ -0,0 +1,61 @@
+<?php
+include_once './db-connect.php';
+include_once './functions.php';
+
+session_start();
+
+if (isset($_POST['street'], $_POST['zipcode'], $_POST['name'], $_POST['category'],
+	$_POST['delivers'])) {
+
+	$street = filter_input(INPUT_POST, 'street', FILTER_SANITIZE_STRING);
+	$zipcode = filter_input(INPUT_POST, 'zipcode', FILTER_SANITIZE_STRING);
+	$name = filter_input(INPUT_POST, 'name', FILTER_SANITIZE_STRING);
+	$category = filter_input(INPUT_POST, 'category', FILTER_SANITIZE_STRING);
+	$delivers = filter_input(INPUT_POST, 'delivers', FILTER_SANITIZE_STRING);
+
+
+	$prep_stmt = "SELECT id FROM locations WHERE street = ? AND zip = ?";
+	$stmt = $mysql_con->prepare($prep_stmt);
+
+	$location_id;
+
+	if ($stmt) {
+		$stmt->bind_param('si', $street, $zipcode);
+		$stmt->execute();
+		$stmt->store_result();
+
+		if ($stmt->num_rows == 1) {
+			// We have the id
+			$stmt->bind_result($location_id);
+			$stmt->fetch();
+
+		}
+
+		else {
+			// we manufacture a location
+			$prep_stmt = "INSERT INTO locations (street, zip) VALUES(?, ?)";
+			$stmt = $mysql_con->prepare($prep_stmt);
+
+			if ($stmt) {
+				$stmt->bind_param('si', $street, $zipcode);
+				$stmt->execute();
+				$location_id = mysqli_insert_id($mysql_con);
+			}
+
+		}
+	}
+
+	$prep_stmt = "INSERT INTO restaurants (name, location, category, delivers, approved, requester)
+		VALUES(?, ?, ?, ?, ?, ?)";
+	$stmt = $mysql_con->prepare($prep_stmt);
+
+	$uid = $_SESSION['user_id'];
+	$approved = 0;
+
+	if ($stmt) {
+		$stmt->bind_param('siiiii', $name, $location_id, $category, $delivers, $approved, $uid);
+		$stmt->execute();
+
+		header('Location: /');
+	}
+}
